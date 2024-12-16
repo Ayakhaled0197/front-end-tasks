e6d16b14f2233388ea6079ac9bc81dad3ea2242a
MahMooD-700 <elmahmudix@gmail.com>
2024-12-16 19:15:28 +0200

    Add files via upload

---
+++ MahMooD/JS 2.js
@@ -0,0 +1,26 @@
+                 // score for an Exam .
+var res = prompt("What is Your Exam Score ?")
+if(res>= 90 && res == 100)
+    { console.log("Excllent");}
+else if (res== 89 && res >= 80)
+    {console.log("Very Good");}
+else if (res== 79 && res >= 60)
+    {console.log("Good");}
+else if (res== 59 && res >= 50)
+    {console.log("Acceptable");}
+else if (res< 50)
+    {console.log("Fail");}
+else if(res< 0 || res > 100)
+    { console.log("Are you Jooking ! 🎃");}
+
+
+
+                     //example about AGe .
+var age = prompt("How Old Are You ?")
+if(age >=14 && age <= 19)
+    {console.log("Hello Teenager 👨‍🦱")}
+else if(age<= 13 && age >= 10){console.log("Hello Boy 👦")}
+else if(age<= 9 && age >= 5){console.log("Hello Kid 👼")}
+
+
+
